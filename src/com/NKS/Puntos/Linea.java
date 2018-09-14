f2082d2f3dcb1bc5cc15ae5ac4faba0c51486045
Kei98 <keisychaves@hotmail.com>
2018-09-13 18:56:52 -0600

    [más cerca]

--- src/com/NKS/Puntos/Linea.java
+++ src/com/NKS/Puntos/Linea.java
@@ -11,41 +11,45 @@ public class Linea {
 	}
 	public void Point(List lineas,int pos,Linea Turn, Linea conexion) {
 		if (pos<lineas.getLenght()){
+			System.out.println("11");
 			Linea line = (Linea) lineas.getElement(pos);
-			if (Turn.Punto2==line.Punto1) {
+			if (Turn.Punto2==line.Punto1 || Turn.Punto1 == line.Punto2) {
+//				Una sola dirección para la prueba, luego lo vemos
 				conexion= new Linea(Turn.Punto1,line.Punto2);
-				
+				System.out.println("12");
 			
 				
 				
-			}if (Turn.Punto1==line.Punto2) {
-				conexion= new Linea(line.Punto1,Turn.Punto2);
-				
-				
+//			}if (Turn.Punto1==line.Punto2) {
+//				conexion= new Linea(line.Punto1,Turn.Punto2);
+//				System.out.println("13");
+//				
 			}else {
 				Point(lineas,++pos,Turn,conexion);
-				
+				System.out.println("14");
 			}
 		} else {
 			System.out.println("no hay punto");
 		}
 	}
-	public void point2(List lineas,int pos, Linea conexion) {
-		if (pos<lineas.getLenght()) {
-			Linea line= (Linea) lineas.getElement(pos);
-			if(conexion.Punto1!=line.Punto2) {
-				if (conexion.Punto2==line.Punto1) {
-					conexion.Punto2=line.Punto2;
-					point2(lineas,++pos,conexion);
-					
-				} else {
-				System.out.println("hay punto");
-					}
-				}
-		}else {
-			System.out.println("no hay punto");
-		}
-
-	}	
+//	public void point2(List lineas,int pos, Linea conexion) {
+//		if (pos<lineas.getLenght()) {
+//			Linea line= (Linea) lineas.getElement(pos);
+//			System.out.println("21");
+//			if(conexion.Punto1!=line.Punto2) {
+//				System.out.println("22");
+//				if (conexion.Punto2==line.Punto1) {
+//					conexion.Punto2=line.Punto2;
+//					point2(lineas,++pos,conexion);
+//					System.out.println("23");
+//				} else {
+//				System.out.println("hay punto");
+//					}
+//				}
+//		}else {
+//			System.out.println("no hay punto");
+//		}
+//
+//	}	
 
 }
--- src/com/NKS/Puntos/Mainpuntos.java
+++ src/com/NKS/Puntos/Mainpuntos.java
@@ -9,15 +9,17 @@ public class Mainpuntos {
 	Punto1.add(1);
 	Punto1.add(1);
 	List Punto2 = new List<>();
-	Punto1.add(2);
-	Punto1.add(1);
+	Punto2.add(1);
+	Punto2.add(2);
 	List Punto3 = new List<>();
-	Punto1.add(2);
-	Punto1.add(1);
+	Punto3.add(2);
+	Punto3.add(1);
 	List Punto4 = new List<>();
-	Punto1.add(2);
-	Punto1.add(2);
+	Punto4.add(2);
+	Punto4.add(2);
 
+//	Me di cuenta que todo se lo añadía a "Punto1", lo cambié pero sigue parecido
+	
 	List Malla = new List<>();
 	Malla.add(Punto1);
 	Malla.add(Punto2);
@@ -25,17 +27,19 @@ public class Mainpuntos {
 	Malla.add(Punto4);
 	
 	MakeLine(Punto1,Punto2);
+	System.out.println("holo");
 	MakeLine(Punto2,Punto3);
+	System.out.println("holo");
 	MakeLine(Punto3,Punto1);
-	System.out.println("hola");
 	
 	
 	
 	}
 	public static void MakeLine(List Punto1,List Punto2) {
 		Linea jugada=new Linea(Punto1,Punto2);
-		jugada.Point(Lineas,0,jugada, null);
 		Lineas.add(jugada);
+		jugada.Point(Lineas,0,jugada, null);
+//		Lineas.add(jugada);
 		
 	}
 
